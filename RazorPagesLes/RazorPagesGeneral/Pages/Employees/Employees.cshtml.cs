51f1f06315079317ce13b420df57f30545f60bd5
Timur <tima.tuhvatshin@mail.ru>
2023-06-14 23:41:24 +0300

    Razor

---
+++ RazorPagesLes/RazorPagesGeneral/Pages/Employees/Employees.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesGeneral.Pages.Employees
+{
+    public class EmployeesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
